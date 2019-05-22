a47559cc716a790a4a459cd2dedc9cb9059871b0
Mattwalsh155 <41699810+Mattwalsh155@users.noreply.github.com>
2019-05-21 21:04:38 -0400

    Tuesday stuff

--- app/src/main/java/com/example/minotaurgame/GameActivity.java
+++ app/src/main/java/com/example/minotaurgame/GameActivity.java
@@ -12,6 +12,9 @@ import android.view.animation.Animation;
 import android.view.animation.TranslateAnimation;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.q42.android.scrollingimageview.ScrollingImageView;
 
 import java.util.Timer;
 
@@ -20,23 +23,40 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     Button Button1;
     Button Button2;
     Button Button3;
-    AnimationDrawable runningMinotaur;
-    AnimationDrawable attackingMinotaur;
-    AnimationDrawable slidingMinotaur;
-    AnimationDrawable jumpingMinotaur;
-    AnimationDrawable fallingMinotaur;
-    ImageView minotaurWalk;
-    ImageView minotaurJump;
-    ImageView minotaurAttack;
-    ImageView minotaurSlide;
-    ImageView minotaurFall;
+    Button pauseButton;
+    //
+    AnimationDrawable minotaurState;
+    //
+    ImageView minotaurImageView;
+    //
+    TextView scoreText;
+    TextView levelText;
+    //
+    int currentScore = 0;
+    int currentLevel = 1;
+    //
+    int screenWidth;
+    int screenHeight;
+    //
     private Handler myHandler;
     private int animation = 0;
     private boolean buttonPressed = true;
     private int loopTime = 900;
 
-    private float jumpXVelocity;
     private Timer timer = new Timer();
+    private Handler handler = new Handler();
+
+    ScrollingImageView scrollingBackground;
+
+    long startTime = System.currentTimeMillis();
+    long elapsedTime = 0;
+
+    boolean isGameOver = false;
+
+    private boolean isPaused = false;
+
+    Thread ourThread = null;
+    volatile boolean playingGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,103 +67,164 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         setContentView(R.layout.activity_game);
 
-        minotaurWalk = (ImageView)findViewById(R.id.playerWalkAnim);
-        minotaurWalk.setImageResource(R.drawable.runningminotaur);
-        runningMinotaur = (AnimationDrawable)minotaurWalk.getDrawable();
-        runningMinotaur.start();
+        minotaurImageView = (ImageView)findViewById(R.id.playerWalkAnim);
+        minotaurImageView.setImageResource(R.drawable.runningminotaur);
+        minotaurState = (AnimationDrawable)minotaurImageView.getDrawable();
+        minotaurState.start();
 
         Button1 = (Button)findViewById(R.id.jumpButton);
         Button2 = (Button)findViewById(R.id.attackButton);
         Button3 = (Button)findViewById(R.id.slideButton);
+        pauseButton = (Button)findViewById(R.id.pauseButton);
 
         Button1.setOnClickListener(this);
         Button2.setOnClickListener(this);
         Button3.setOnClickListener(this);
+        pauseButton.setOnClickListener(this);
+
+        scrollingBackground = (ScrollingImageView)findViewById(R.id.scrolling_background);
+        scrollingBackground.setSpeed(3);
+        int enemiesKilled = 0;
+
+        scoreText = (TextView)findViewById(R.id.scoreText);
+        levelText = (TextView)findViewById(R.id.levelText);
+
+        // Get screen size
+//        WindowManager wm = getWindowManager();
+//        Display disp = wm.getDefaultDisplay();
+//        Point size = new Point();
+//        disp.getSize(size);
+//        screenWidth = size.x;
+//        screenHeight = size.y;
+
+        //Start the timer
+//        timer.schedule(new TimerTask(){
+//            @Override
+//            public void run() {
+//                handler.post(new Runnable() {
+//                    @Override
+//                    public void run() {
+//
+//                   }
+//                });
+//            }
+//        }, 0, 20);
 
 
-        if(buttonPressed){
-            myHandler = new Handler() {
-                public void handleMessage(Message msg) {
-                    super.handleMessage(msg);
+        myHandler = new Handler() {
 
-                    switch(animation){
+            public void handleMessage(Message msg) {
+                super.handleMessage(msg);
+
+                if (!isPaused) {
+
+                    switch (animation) {
                         case 0:
-                            minotaurWalk = (ImageView)findViewById(R.id.playerWalkAnim);
-                            minotaurWalk.setImageResource(R.drawable.runningminotaur);
-                            runningMinotaur = (AnimationDrawable)minotaurWalk.getDrawable();
-                            runningMinotaur.start();
-                            loopTime = 900;
+                            SetAnimation(R.drawable.runningminotaur, 900, false, false);
+                            scrollingBackground.setSpeed(3);
                             break;
 
                         case 1:
-                            minotaurAttack = (ImageView) findViewById(R.id.playerWalkAnim);
-                            minotaurAttack.setImageResource(R.drawable.attackingminotaur);
-                            attackingMinotaur = (AnimationDrawable) minotaurAttack.getDrawable();
-                            attackingMinotaur.start();
-                            loopTime = 1600;
+                            SetAnimation(R.drawable.attackingminotaur, 1600, false, false);
                             break;
 
                         case 2:
-                            minotaurSlide = (ImageView) findViewById(R.id.playerWalkAnim);
-                            minotaurSlide.setImageResource(R.drawable.slidingminotaur);
-                            slidingMinotaur = (AnimationDrawable) minotaurSlide.getDrawable();
-                            slidingMinotaur.start();
-                            loopTime = 600;
+                            SetAnimation(R.drawable.slidingminotaur, 1200, false, false);
+                            scrollingBackground.setSpeed(10);
                             break;
 
                         case 3:
-                            minotaurJump = (ImageView) findViewById(R.id.playerWalkAnim);
-                            minotaurJump.setImageResource(R.drawable.jumpingminotaur);
-                            jumpingMinotaur = (AnimationDrawable) minotaurJump.getDrawable();
-                            jumpingMinotaur.start();
-                            loopTime = 800;
-                            moveAnimationUp();
-                            //moveAnimationDown();
-
+                            SetAnimation(R.drawable.jumpingminotaur, 800, true, false);
                             break;
 
                         case 4:
-                            minotaurFall = (ImageView) findViewById(R.id.playerWalkAnim);
-                            minotaurFall.setImageResource(R.drawable.fallingminotaur);
-                            fallingMinotaur = (AnimationDrawable) minotaurFall.getDrawable();
-                            fallingMinotaur.start();
-                            loopTime = 800;
-                            //moveAnimationUp();
-                            moveAnimationDown();
-
+                            SetAnimation(R.drawable.fallingminotaur, 800, false, true);
                             break;
 
                         default:
-                            minotaurWalk = (ImageView)findViewById(R.id.playerWalkAnim);
-                            minotaurWalk.setImageResource(R.drawable.runningminotaur);
-                            runningMinotaur = (AnimationDrawable)minotaurWalk.getDrawable();
-                            runningMinotaur.start();
-                            loopTime = 900;
+                            SetAnimation(R.drawable.runningminotaur, 900, false, false);
                             break;
 
                     }
 
-                    if(animation == 3){
+                    if (animation == 3) {
                         animation = 4;
                     } else {
                         animation = 0;
                     }
-
-
-                    myHandler.sendEmptyMessageDelayed(0, loopTime);
-                    buttonPressed = false;
                 }
-            };
-            myHandler.sendEmptyMessage(0);
+
+
+                myHandler.sendEmptyMessageDelayed(0, loopTime);
+                buttonPressed = false;
+
+            }
+        };
+        //I'm not sure where to put this to make it work
+        //updateScore(enemiesKilled);
+        updateScore(enemiesKilled);
+        updateLevel();
+
+        myHandler.sendEmptyMessage(0);
+    }
+
+    public void SetAnimation(int id, int lt, boolean goUp, boolean goDown) {
+        minotaurImageView = findViewById(R.id.playerWalkAnim);
+        minotaurImageView.setImageResource(id);
+        minotaurState = (AnimationDrawable) minotaurImageView.getDrawable();
+        minotaurState.start();
+        loopTime = lt;
+
+        if (goUp) {
+            moveAnimationUp();
+        }
+
+        if (goDown) {
+            moveAnimationDown();
         }
     }
+
+
+//    public void pausePushed(View view) {
+//        if (isPaused == false) {
+//            isPaused = true;
+//
+//            //stop the timer
+//            timer.cancel();
+//            timer = null;
+//
+//            //change button text
+//            pauseButton.setText("Resume");
+//        } else {
+//            isPaused = false;
+//
+//            //change button text back
+//            pauseButton.setText("Pause");
+//
+//            //create and start the timer
+//            timer = new Timer();
+//            timer.schedule(new TimerTask(){
+//                @Override
+//                public void run() {
+//                    handler.post(new Runnable() {
+//                        @Override
+//                        public void run() {
+//
+//                        }
+//                    });
+//                }
+//            }, 0, 20);
+//        }
+//    }
+
+
 
     public void moveAnimationUp() {
         Animation img = new TranslateAnimation(Animation.ABSOLUTE, Animation.ABSOLUTE, 0, -300);
         img.setDuration(800);
 
 
-        minotaurJump.startAnimation(img);
+        minotaurImageView.startAnimation(img);
         //myHandler.sendEmptyMessageDelayed(0, 800);
         //moveAnimationDown();
     }
@@ -153,7 +234,8 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         img.setDuration(800);
 
 
-        minotaurJump.startAnimation(img);
+
+        minotaurImageView.startAnimation(img);
     }
 
     @Override
@@ -162,41 +244,110 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
 
         switch (v.getId()) {
             case R.id.attackButton:
-//                ImageView minotaurAttack = (ImageView) findViewById(R.id.playerWalkAnim);
-//                minotaurAttack.setImageResource(R.drawable.attackingminotaur);
-//                attackingMinotaur = (AnimationDrawable) minotaurAttack.getDrawable();
-//                attackingMinotaur.start();
+//
                 buttonPressed = true;
                 animation = 1;
+                //scrollingBackground.setSpeed(3);
                 break;
 
             case R.id.jumpButton:
                 buttonPressed = true;
                 animation = 3;
+                //scrollingBackground.setSpeed(3);
                 //moveAnimation();
 
                 break;
 
             case R.id.slideButton:
-//                ImageView minotaurSlide = (ImageView) findViewById(R.id.playerWalkAnim);
-//                minotaurSlide.setImageResource(R.drawable.slidingminotaur);
-//                slidingMinotaur = (AnimationDrawable) minotaurSlide.getDrawable();
-//                slidingMinotaur.start();
+//
                 animation = 2;
                 buttonPressed = true;
+                //scrollingBackground.setSpeed(10);
+               /* if(animation == 2) {
+                    scrollingBackground.setSpeed(10);
+                } else {
+                    scrollingBackground.setSpeed(1);
+                }*/
+                break;
+
+            case R.id.pauseButton:
+                if (isPaused) {
+                    minotaurState.start();
+                    scrollingBackground.start();
+                    // *** Use "minotaurImageView" and try to stop the animation
+
+                    isPaused = false;
+
+                    pauseButton.setText("Pause");
+                    Button1.setEnabled(true);
+                    Button2.setEnabled(true);
+                    Button3.setEnabled(true);
+                } else {
+                    minotaurState.stop();
+                    scrollingBackground.stop();
+
+                    isPaused = true;
+
+                    pauseButton.setText("Resume");
+                    Button1.setEnabled(false);
+                    Button2.setEnabled(false);
+                    Button3.setEnabled(false);
+                }
 
                 break;
 
             default:
-//                ImageView minotaurWalk = (ImageView)findViewById(R.id.playerWalkAnim);
-//                minotaurWalk.setImageResource(R.drawable.runningminotaur);
-//                runningMinotaur = (AnimationDrawable)minotaurWalk.getDrawable();
-//                runningMinotaur.start();
                 animation = 0;
                 buttonPressed = true;
-
+                //scrollingBackground.setSpeed(3);
                 break;
         }
+
+    }
+
+    void updateScore(int enemiesKilled) {
+
+        for (int i = 1; i <= enemiesKilled; i++) {
+            currentScore = currentScore + i * 10;
+        }
+
+        scoreText.setText("Score: " + currentScore);
+    }
+
+    void updateLevel() {
+        if (elapsedTime >= 10000) {
+            currentLevel++;
+        }
+        levelText.setText("Level: " + currentLevel);
+
+    }
+
+//    public void pause() {
+//        playingGame = false;
+//        try {
+//            ourThread.join();
+//        } catch (InterruptedException e) {
+//
+//        }
+//    }
+//
+//    public void resume() {
+//        playingGame = true;
+//        ourThread = new Thread(this);
+//        ourThread.start();
+//    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        elapsedTime = elapsedTime + (System.currentTimeMillis() - startTime);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startTime = (System.currentTimeMillis());
+
     }
 
     @Override
