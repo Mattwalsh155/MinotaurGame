78977e44b13558414394cd7c6d8b8a6975b288d1
Ace Dragonair <41645417+acedragonair@users.noreply.github.com>
2019-05-23 19:48:58 -0400

    Update Controls.java

--- app/src/main/java/com/example/minotaurgame/Controls.java
+++ app/src/main/java/com/example/minotaurgame/Controls.java
@@ -2,16 +2,17 @@ package com.example.minotaurgame;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.view.Window;
+import android.view.WindowManager;
 
-public class Controls extends AppCompatActivity {
+public class controls extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_controls);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
     }
-
-
-
 }
